be5eb40eded4c5730e449d3d69d823555c1a14b3
Melinda Mizuta <mkmizuta@gmail.com>
2013-10-29 22:49:26 -0700

    first commit

---
+++ question1.rb
@@ -0,0 +1,10 @@
+puts 'What\'s your first name?'
+first = gets.chomp 
+
+puts 'What\'s your middle name?'
+middle = gets.chomp
+
+puts 'What\'s your last name?'
+last = gets.chomp
+
+puts 'Well it\'s a pleasure meeting you, ' + first + ' ' + middle + ' ' + last +'!'
---
+++ question2.rb
@@ -0,0 +1,3 @@
+puts 'What\'s your favorite number?'
+number = gets.chomp.to_i + 1
+puts 'How about an extra number for luck: ' + number.to_s + '.'
